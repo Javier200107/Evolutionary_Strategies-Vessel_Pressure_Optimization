b766966a57dbf360e87ab9bf84274f1fc156651f
Javier200107 <xaviergonzalezbejar@gmail.com>
2023-12-28 13:36:38 +0100

    Algorithm complete

--- evolution_strategies.py
+++ evolution_strategies.py
@@ -1,10 +1,11 @@
-import numpy as np
+import numpy as np, operator
 from deap import base, creator, tools, algorithms
+from functools import partial
 
 # Function to initialize an individual with strategy
 def init_individual(icls, scls):
     n = 4
-    upper_bound_values = [6.1875, 6.1875, 200, 240]
+    upper_bound_values = [6.1875, 6.1875, 100, 200]
     lower_bound_values = [0.0625, 0.0625, 10, 10]
     upper_bound_strategy = [1, 1, 5, 10]
     lower_bound_strategy = [0.1, 0.1, 1, 1]
@@ -38,10 +39,27 @@ def feasibility(individual):
 # Distance function for penalizing infeasible individuals
 def distance(individual):
     if any(g > 0 for g in constraints(individual)):
-        return 100000000
+        return 1e8
 
+# Filter infeasible individuals
+def filter_infeasible(population, function, penal=1e8):
+    return function([ind[0] for ind in population if ind[0] < penal])
+
+# Check strategy values
+def checkStrategy(minstrategy):
+    def decorator(func):
+        def wrappper(*args, **kargs):
+            children = func(*args, **kargs)
+            for child in children:
+                for i, s in enumerate(child.strategy):
+                    if s < minstrategy[i]:
+                        child.strategy[i] = minstrategy[i]
+            return children
+        return wrappper
+    return decorator
+    
 # Plotting statistics
-def plot_and_save_statistics(logbook, output_dir="output"):
+def plot_and_save_statistics(logbook, output_dir="output", params=None):
     
     import pickle, os, datetime, matplotlib.pyplot as plt
 
@@ -63,18 +81,24 @@ def plot_and_save_statistics(logbook, output_dir="output"):
     ax1.set_xlabel("Generation")
     ax1.set_ylabel("Fitness", color="b")
     for tl in ax1.get_yticklabels():
-        tl.set_color("b")
+        tl.set_color("b")  
+    # Use log scale for y-axis
+    ax1.set_yscale('log')
 
-    ax2 = ax1.twinx()
-    line2 = ax2.plot(gen, size_avgs, "r-", label="Average Size")
-    ax2.set_ylabel("Size", color="r")
-    for tl in ax2.get_yticklabels():
-        tl.set_color("r")
+    ax2 = ax1
+    line2 = ax2.plot(gen, size_avgs, "r-", label="Average Fitness")
+    # Use log scale for y-axis
+    ax2.set_yscale('log')
 
     lns = line1 + line2
     labs = [l.get_label() for l in lns]
     ax1.legend(lns, labs, loc="center right")
 
+    if params is not None:
+        str_params = " - ".join([f"{key}: {value}" for key, value in params.items()])
+        plt.title("Fitness over Generations\n"+str_params)
+    else:
+        plt.title("Fitness over Generations\n ")
     plt.show()
 
     # Save the figure in the output folder
@@ -92,7 +116,8 @@ if __name__ == "__main__":
     toolbox = base.Toolbox()
     toolbox.register("individual", init_individual, creator.Individual, creator.Strategy)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
+    toolbox.register("mutate", tools.mutESLogNormal, c=1.0, indpb=0.3)
+    toolbox.decorate("mutate", checkStrategy([0.1, 0.1, 1, 1]))
     toolbox.register("select", tools.selTournament, tournsize=3)
 
     # Register the evaluation function and apply penalty for infeasibility
@@ -101,14 +126,20 @@ if __name__ == "__main__":
 
     # Statistics
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("min", np.min)
-    stats.register("max", np.max)
-    stats.register("std", np.std)
+
+    stats.register("avg", partial(filter_infeasible, function=np.mean))
+    stats.register("min", partial(filter_infeasible, function=np.min))
+    stats.register("max", partial(filter_infeasible, function=np.max))
+    stats.register("std", partial(filter_infeasible, function=np.std))
+    stats.register("median", partial(filter_infeasible, function=np.median))
+    stats.register("feasibles", partial(filter_infeasible, function=len))
+    stats.register("unique_ind", partial(filter_infeasible, function=lambda x: len(np.unique(x))))
 
     # Evolutionary Strategies (ES) Algorithm parameters
     MU, LAMBDA_ = 1000, 1000
-    MUTPB, NGEN = 0.9, 100
+    MUTPB, NGEN = 0.8, 500
+    ELITISM_RATIO = 0.1
+    ELITISM = int(LAMBDA_*ELITISM_RATIO)
 
     # Initial population
     pop = toolbox.population(n=MU)
@@ -116,7 +147,7 @@ if __name__ == "__main__":
     # Evolutionary process and logging
     pop, logbook = algorithms.eaMuCommaLambda(pop, toolbox, mu=MU, lambda_=LAMBDA_,
                                             cxpb=0, mutpb=MUTPB, ngen=NGEN, stats=stats,
-                                            halloffame=None, verbose=True)
+                                            halloffame=tools.HallOfFame(ELITISM, lambda x, y: (x == y).all()), verbose=True)
     # Display results
     print("Best individual after", NGEN, "generations:")
     best_ind = tools.selBest(pop, 1)[0]
@@ -124,10 +155,14 @@ if __name__ == "__main__":
     arr_fitness = [ind.fitness.values[0] for ind in pop]
     arr_fitness = [x for x in arr_fitness if x < 100000000]
     print("Fitness value (Mean):", np.mean(arr_fitness))
-    print("Fitness value (Worst):", np.max([ind.fitness.values[0] for ind in pop]))
-
+    print("Fitness value (Median):", np.median(arr_fitness))
+    print("Fitness value (Worst):", np.max(arr_fitness))
+    print("Number of feasible individuals:", len(arr_fitness))
+    print("Number of infeasible individuals:", len(pop) - len(arr_fitness))
+    print("Standard deviation:", np.std(arr_fitness))
+    print("Number of unique individuals:", len(np.unique(arr_fitness)))
     print("Individual variables:", best_ind)
     print("Constraints:", constraints(best_ind))
 
     # Plot statistics
-    plot_and_save_statistics(logbook)
+    plot_and_save_statistics(logbook, params={"MU": MU, "LAMBDA": LAMBDA_, "MUTPB": MUTPB, "NGEN": NGEN, "ELITISM_RATIO": ELITISM_RATIO})
